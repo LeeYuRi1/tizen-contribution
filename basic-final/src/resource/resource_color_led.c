a71ef48e8e855026adc7c3a92a626dc2372a2467
LeeYuRi1 <33548856+LeeYuRi1@users.noreply.github.com>
2018-10-10 01:14:25 +0900

    코딩 룰 오류 수정

--- basic-final/src/resource/resource_color_led.c
+++ basic-final/src/resource/resource_color_led.c
@@ -7,20 +7,20 @@ int resource_write_color_led(int pin_num[], int value)
 {
 	for (int i = 0; i < 3; i++) {
 		switch (i) {
-			case 0:
-				resource_write_led(pin_num[0],value);
-				delay(2000);
-				break;
-			case 1:
-				resource_write_led(pin_num[1],value);
-				delay(2000);
-				break;
-			case 2:
-				resource_write_led(pin_num[2],value);
-				delay(2000);
-				break;
-			default:
-				break;
+		case 0:
+			resource_write_led(pin_num[0],value);
+			delay(2000);
+			break;
+		case 1:
+			resource_write_led(pin_num[1],value);
+			delay(2000);
+			break;
+		case 2:
+			resource_write_led(pin_num[2],value);
+			delay(2000);
+			break;
+		default:
+			break;
 		}
 	}
 	return 0;
