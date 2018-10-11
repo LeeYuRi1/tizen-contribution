fe3fe8f0a1faabe16f8417403f5c1f84cc34fc11
LeeYuRi1 <33548856+LeeYuRi1@users.noreply.github.com>
2018-10-11 19:59:11 +0900

    변수명 수정

--- basic-final/src/resource/resource_color_led.c
+++ basic-final/src/resource/resource_color_led.c
@@ -3,20 +3,20 @@
 #include "log.h"
 
 //r,g,b 색상의 빛을 연속으로 빛나게 하는 코드 
-int resource_write_color_led(int pin_num[], int value)
+int resource_write_color_led(int color_num[], int value)
 {
 	for (int i = 0; i < 3; i++) {
 		switch (i) {
 		case 0:
-			resource_write_led(pin_num[0],value);
+			resource_write_led(color_num[0],value);
 			delay(2000);
 			break;
 		case 1:
-			resource_write_led(pin_num[1],value);
+			resource_write_led(color_num[1],value);
 			delay(2000);
 			break;
 		case 2:
-			resource_write_led(pin_num[2],value);
+			resource_write_led(color_num[2],value);
 			delay(2000);
 			break;
 		default:
